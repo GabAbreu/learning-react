ba5e5a3ffb4fa8a2f3e4ed7de7954267509c557b
Gabriel Abreu <gabriel.abreu9913@gmail.com>
2022-08-23 03:42:18 -0300

    Added new style
    23. Build a React info site | Project Setup
    [video](https://youtu.be/bMknfKXIFA8?t=6801)

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,12 +6,13 @@ export default function Header(){
         <>
             <header className="header">
                 <nav className="nav-container">
-                    <img className="nav-logo" src={reactLogo}/>
-                    <ul className="nav-items">
-                        <li><a className="nav-link" href="https://www.google.com.br/">Pricing</a></li>
-                        <li><a className="nav-link" href="https://www.google.com.br/">About</a></li>
-                        <li><a className="nav-link" href="https://www.google.com.br/">Contact</a></li>
-                    </ul>
+                    <div className="nav-logo">
+                        <img src={reactLogo}/>
+                        <p>ReactFacts</p>
+                    </div>
+                    <div className="nav-title">
+                        <p>React Course - Project 1</p>
+                    </div>
                 </nav>
             </header>
         </>
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,14 +1,18 @@
 import React from "react"
+import reactLogo from "../img/react-logo.png"
 
 export default function MainContent(){
     return(
         <div className="main-content">
-            <h1>Reasons why i'm learning React</h1>
-            <ol>
-                <li>Faster than vanilla html/css/javascript</li>
-                <li>Easy to learn</li>
-                <li>Updated for Skilled people</li>
-            </ol>
+            <h1>Fun facts about React</h1>
+            <ul className="content-items">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K starts on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Power thousand of enterprise apps, including mobile apps</li>
+            </ul>
+            <img src={reactLogo} ></img>
         </div>
     )
 }
